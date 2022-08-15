ef788538856b71f26956625c71a1eb2f66c34692
rch20 <reemshalak22@hotmail.com>
2022-08-15 15:52:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const writeCards = (names, eventName) => {
+  let thankYouMessage = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessage[
+      i
+    ] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+  }
+
+  return thankYouMessage;
+};
+
+const countDown = (number) => {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+};
